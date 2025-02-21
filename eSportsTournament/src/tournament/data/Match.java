20b51767d17a95d3510b0047f9d112d8adb64e1d
Miguel26-cat <migtorlop@alu.edu.gva.es>
2025-02-21 17:38:33 +0100

    2. Creating TournamentManager - Start - Miguel

--- eSportsTournament/src/tournament/data/Match.java
+++ eSportsTournament/src/tournament/data/Match.java
@@ -2,48 +2,55 @@ package tournament.data;
 
 public class Match
 {
-    Tournament tournament;
-    Player participant1;
-    Player participant2;
-    String result = "Pending";
+    private Tournament tournament;
+    private Player participant1;
+    private Player participant2;
+    private String result = "Pending";
 
-    public Match(Tournament tournament,Player parcipant1,Player participant2,String result)
+    public Match(Tournament tournament,Player participant1,Player participant2)
     {
         this.tournament = tournament;
         this.participant1 = participant1;
         this.participant2 = participant2;
-        this.result = result;
     }
 
-    public Tournament getTournament() {
+    public Tournament getTournament()
+    {
         return tournament;
     }
 
-    public Player getParticipant1() {
+    public Player getParticipant1()
+    {
         return participant1;
     }
 
-    public Player getParticipant2() {
+    public Player getParticipant2()
+    {
         return participant2;
     }
 
-    public String getResult() {
+    public String getResult()
+    {
         return result;
     }
 
-    public void setTournament(Tournament tournament) {
+    public void setTournament(Tournament tournament)
+    {
         this.tournament = tournament;
     }
 
-    public void setParticipant1(Player participant1) {
+    public void setParticipant1(Player participant1)
+    {
         this.participant1 = participant1;
     }
 
-    public void setParticipant2(Player participant2) {
+    public void setParticipant2(Player participant2)
+    {
         this.participant2 = participant2;
     }
 
-    public void setResult(String result) {
+    public void setResult(String result)
+    {
         this.result = result;
     }
 
--- eSportsTournament/src/tournament/main/TournamentManager.java
+++ eSportsTournament/src/tournament/main/TournamentManager.java
@@ -1,6 +1,46 @@
 package tournament.main;
 
+import tournament.data.*;
+
 public class TournamentManager
 {
+    Player[] registeredPlayer = new Player[20];
+    Team [] registeredTeam = new Team[10];
+    Tournament [] registeredTournament = new Tournament[6];
+    Match [] registeredMatch = new Match[4];
 
+    public void initialize()
+    {
+        initializePlayers();
+
+
+
+
+
+    }
+    public void initializePlayers()
+    {
+        registeredPlayer[0] = new Player("Alejandro",12,453.1f);
+        registeredPlayer[1] = new Player("Valeria",19,483.1f);
+        registeredPlayer[2] = new Player("Mateo",52,899.0f);
+        registeredPlayer[3] = new Player("Isabella",91,91.45f);
+        registeredPlayer[4] = new Player("Santiago",23,532.2f);
+        registeredPlayer[5] = new Player("Camila",99,222.2f);
+        registeredPlayer[6] = new Player("Leonardo",38,753.3f);
+        registeredPlayer[7] = new Player("Sofía",71,463.99f);
+        registeredPlayer[8] = new Player("Andrés",88,441.1f);
+        registeredPlayer[9] = new Player("Elena",4,82701.54f);
+    }
+    public void initializeTeam()
+    {
+
+    }
+    public void initializeTournament()
+    {
+
+    }
+    public void initializeMatch()
+    {
+
+    }
 }
