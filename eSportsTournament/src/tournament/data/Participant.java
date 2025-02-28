fc956d2643a185a0d0ee1e0681bf4286a542ba3b
Adrian Vico <adrviclop@alu.edu.gva.es>
2025-02-28 17:49:20 +0100

    Adding comments for JavaDoc Adrian Vico

--- eSportsTournament/src/tournament/data/Participant.java
+++ eSportsTournament/src/tournament/data/Participant.java
@@ -1,3 +1,8 @@
+/**
+ * @author Adrian
+ * This class it is the parent of the Player and the Team classes
+ */
+
 package tournament.data;
 
 public abstract class Participant {
@@ -7,14 +12,23 @@ public abstract class Participant {
         this.name = name;
     }
 
+    /**
+     * @return This returns the name of the participant.
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String playerName) {
-        name = playerName;
+    /**
+     * @param name This sets the name of the new team or player.
+     */
+    public void setName(String name) {
+        this.name = name;
     }
 
+    /**
+     * @return This returns the name of the participant, which is used in the children classes.
+     */
     @Override
     public String toString() {
         return name;
--- eSportsTournament/src/tournament/exceptions/GameModeException.java
+++ eSportsTournament/src/tournament/exceptions/GameModeException.java
@@ -1,3 +1,8 @@
+/**
+ * @author Adrian
+ * This exception checks if the game mode is the correct one (1v1 or 5v5).
+ */
+
 package tournament.exceptions;
 
 public class GameModeException extends Exception {
